7026a2ddd34044cd83589e287afea52b7538b43d
lannhi282 <157347909+lannhi282@users.noreply.github.com>
2024-01-22 13:55:46 +0700

    swing_tutorial

---
+++ swing_tutorial/Swing_Tutorial.java
@@ -0,0 +1,12 @@
+package swing_tutorial;
+
+import view.employeeView;
+
+public class Swing_Tutorial {
+
+    public static void main(String[] args) {
+        employeeView empView = new employeeView();
+
+        empView.setVisible(true);
+    }
+}
